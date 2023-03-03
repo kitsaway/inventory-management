a92a9f8961a41765a4dee5fd09f8e6641955b303
mtokhiani <tokhiiani@gmail.com>
2023-03-03 22:11:02 +0400

    updated index file with router

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import AddInventory from "./components/AddInventory";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route index path="/" element={<App />} />
+        <Route path="/add" element={<AddInventory />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
