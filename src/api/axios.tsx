6f5094800d089907fd8ee5f7832c3a1980638286
mtokhiani <tokhiiani@gmail.com>
2023-03-03 22:10:14 +0400

    added api integration

---
+++ src/api/axios.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export interface InventoryInput {
+  name: string;
+  location: string;
+  price: string;
+}
+
+export interface InventoryOutput extends InventoryInput {
+  id: string;
+}
+
+const url: string = process.env.REACT_APP_URL;
+
+export const create = async (formData: InventoryInput) => {
+  const { data } = await axios.post(url, {
+    payload: formData,
+  });
+  return data;
+};
+
+export const deleteById = async (id: string) => {
+  const response = await axios.delete(`${url}/${id}`);
+  return response;
+};
+
+export const getAll = async (page: number, filter: string) => {
+  const { data } = await axios.get(url, {
+    params: {
+      filter,
+      page,
+    },
+  });
+  return data;
+};
