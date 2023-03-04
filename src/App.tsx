455614b733c9292588ced7226791c1ead083285b
mtokhiani <tokhiiani@gmail.com>
2023-03-04 22:08:06 +0400

    fixed placement of provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import PagePagination from "./components/atoms/PagePagination";
 import Header from "./components/Header";
 import InventoryTable from "./components/InventoryTable";
-import { InventoryProvider } from "./context";
 
 const App = (): JSX.Element => {
   return (
-    <InventoryProvider>
+    <>
       <Header />
       <InventoryTable />
       <PagePagination />
-    </InventoryProvider>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { InventoryProvider } from "./context";
 import App from "./App";
 import AddInventory from "./components/AddInventory";
 
@@ -11,11 +12,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <InventoryProvider>
     <Router>
       <Routes>
         <Route index path="/" element={<App />} />
         <Route path="/add" element={<AddInventory />} />
       </Routes>
     </Router>
+    </InventoryProvider>
   </React.StrictMode>
 );
